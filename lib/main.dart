acefd92c7b82a40fe945f2fa35610f17c78cb04e
Vũ Tiến Hai <hai2662@gmail.com>
2022-05-01 02:00:43 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 import 'base/di.dart';
@@ -7,6 +8,8 @@ import 'shared/constants/common.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
   await Firebase.initializeApp();
   await DependencyInjection.init(DEV_ENVIRONMENT);
   runApp(App());
