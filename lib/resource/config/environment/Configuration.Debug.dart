0a8f5ecd871dd8583073630043f0c5c77787fea0
Vũ Tiến Hai <hai2662@gmail.com>
2022-10-26 09:14:10 +0700

    Update

--- lib/resource/config/environment/Configuration.Debug.dart
+++ lib/resource/config/environment/Configuration.Debug.dart
@@ -7,13 +7,16 @@ final Map<String, String> debugAppSettings = {
   MediaApiEnvironment: 'DEV',
   DefaultLanguageCode: 'vi-VN',
   UIAPIDomain: 'https://hico.dev.bluebolt.software/api/',
+  LineChannelId: '1656878196',
+  StripePublishableKey:
+      'pk_test_51Gt542F8AuNSUurPybikIpGB7NGK3HNGszLZ39WgCAZ3JzpzFFYJbHh1l2rNxq9tBnDvcM6Ex4nslrUTji2iJy4Y005VdvVXoN',
   // GraphQLAuthUrl: 'https://hico.co.jp/api/',
   // GraphQLServiceUrl: 'https://hico.co.jp/api/',
   // WebSocketGraphQLUrl: 'wss://hasura.io/learn/graphql',
   // MediaApiEnvironment: 'PROD',
   // DefaultLanguageCode: 'vi-VN',
   // UIAPIDomain: 'https://hico.co.jp/api/',
-  LineChannelId: '1656878196',
-  StripePublishableKey:
-      'pk_test_51Gt542F8AuNSUurPybikIpGB7NGK3HNGszLZ39WgCAZ3JzpzFFYJbHh1l2rNxq9tBnDvcM6Ex4nslrUTji2iJy4Y005VdvVXoN',
+  // LineChannelId: '1656878196',
+  // StripePublishableKey:
+  //     'pk_live_51Gt542F8AuNSUurPdhLJmDtUprwHaDjLlntokyK6GorPlKECgV90UEdFXeflWX1Q7bimvU9H18Scb9LzBFGoz5RX00BG88HNp8',
 };
